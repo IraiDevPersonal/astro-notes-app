3da8d83383647f7c394f10a9da4ebe03a57f4056
Ignacio Arriagada (IraiDev) <dev.iarriagada@gmail.com>
2026-01-07 21:29:43 -0300

    update: se agregaron ids y custom event como constantes para para centraliazr sus valores

---
+++ src/components/sidebar/constants.ts
@@ -0,0 +1,13 @@
+export const SIDEBAR_IDS = {
+  SIDEBAR: "sidebar",
+  OVERLAY: "sidebar-overlay",
+  TRIGGER: "sidebar-trigger",
+  CLOSE: "sidebar-close",
+};
+
+export const SIDEBAR_CUSTOM_EVENTS = {
+  TOGGLE: "sidebar:toggle",
+  OPEN: "sidebar:open",
+  CLOSE: "sidebar:close",
+  TRIGGER_TOGGLE: "sidebar-trigger:toggle",
+};
