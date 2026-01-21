514bffd6d6a8fd2d4732de72e1db12c878a712a5
Ignacio Arriagada (IraiDev) <dev.iarriagada@gmail.com>
2026-01-21 12:49:32 -0300

    feat: Reorganiza las páginas de usuario, moviendo 'Mi Espacio' a 'Inicio', añadiendo 'Favoritas' e integrando el servicio de usuario.

---
+++ src/modules/user/models/me.model.ts
@@ -0,0 +1,10 @@
+export interface MeModel {
+  id: string;
+  name: string;
+  email: string;
+  avatar: string;
+  lastName: string;
+  createdAt: string;
+  ownerResourceCount: number;
+  sharedResourceCount: number;
+}
---
+++ src/modules/user/services/get-me.service.ts
@@ -0,0 +1,8 @@
+import { noteApi } from "@/api/note.api";
+import type { MeModel } from "../models/me.model";
+
+export async function getMeService() {
+  const { data } = await noteApi.get<{ data: MeModel }>("/me");
+
+  return data;
+}
