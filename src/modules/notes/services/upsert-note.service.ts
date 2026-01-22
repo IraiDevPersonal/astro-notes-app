d77ed294f41dcd40c7cb07225a0925be1a053dd0
Ignacio Arriagada (IraiDev) <dev.iarriagada@gmail.com>
2026-01-22 15:54:36 -0300

    refactor:  correcion respuesta d eendpoint para crear notas

--- src/modules/notes/services/upsert-note.service.ts
+++ src/modules/notes/services/upsert-note.service.ts
@@ -3,7 +3,7 @@ import type { NoteModel } from "../models/note.model";
 import type { CreateNoteModel } from "../models/upsert-note.model";
 
 export async function createNoteService(note: CreateNoteModel) {
-  const response = await noteApi.post<{ data: NoteModel }>("/notes", note);
+  const { data } = await noteApi.post<{ data: NoteModel }>("/notes", note);
 
-  return response;
+  return data;
 }
