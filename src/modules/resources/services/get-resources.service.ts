88851f32264b9f1e68290dbd51e8096fb0aa3a6a
Ignacio Arriagada (IraiDev) <dev.iarriagada@gmail.com>
2026-01-28 16:33:19 -0300

    feat: hacer opcional el parámetro `resourceType` en `getResourcesService` y actualizar la página de inicio para obtener todos los recursos por defecto.

--- src/modules/resources/services/get-resources.service.ts
+++ src/modules/resources/services/get-resources.service.ts
@@ -2,11 +2,11 @@ import { noteApi } from "@/api/note.api";
 import type { ResourceModel } from "../models/resource.model";
 import type { ResourceType } from "../models/resource-type.model";
 
-export async function getResourcesService(resourceType: ResourceType) {
+export async function getResourcesService(resourceType?: ResourceType) {
   return await noteApi.get<{ data: ResourceModel }>(
     "/user/550e8400-e29b-41d4-a716-446655440000/resources",
     {
-      params: { type: resourceType },
+      params: resourceType ? { type: resourceType } : undefined,
     },
   );
 }
